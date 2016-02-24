8a8113df31f31c3cff5338f9be41096502376d94
Q <q@MacBook-Pro-de-nhberne.local>
2016-02-24 17:50:46 +0100

    build++

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo "pwd: $(pwd)"
 echo "ls: $(ls)"
+echo "CIRCLE_ARTIFACTS: $CIRCLE_ARTIFACTS"
 curl \
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@app/build/outputs/apk/app-debug.apk" \
+-F "ipa=@$CIRCLE_ARTIFACTS/apk/app-debug.apk" \
 -H "X-HockeyAppToken: 916232226137de25c68eae94ea3a926f" \
 https://rink.hockeyapp.net/api/2/apps/29d2a836383d4fda977697a8754fc5de/app_versions/upload
