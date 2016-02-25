65edb8223d6214e32a1aa84d9122fbe9cf182a80
votre_pseudo <moi@email.com>
2016-02-25 16:52:51 +0100

    logs --

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-echo "pwd: $(pwd)"
-echo "ls: $(ls)"
-echo "CIRCLE_ARTIFACTS: $(ls $CIRCLE_ARTIFACTS)"
-echo "CIRCLE_ARTIFACTS/outputs/apk: $(ls $CIRCLE_ARTIFACTS/outputs/apk)"
 
 curl \
 -F "status=2" \
 -F "notify=1" \
 -F "ipa=@$CIRCLE_ARTIFACTS/outputs/apk/app-debug.apk" \
--H "X-HockeyAppToken: 916232226137de25c68eae94ea3a926f" \
+-H "X-HockeyAppToken: e07db807b6344a6d8e65f1c5a8c0b4eb" \
 https://rink.hockeyapp.net/api/2/apps/29d2a836383d4fda977697a8754fc5de/app_versions/upload
