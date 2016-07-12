8109b0cc745247103215f79066d2dc812e6695a0
Quentin Gaultier <quentin.gaultier@gmail.com>
2016-07-12 11:49:46 +0700

    try fix circleci deployment

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+local APP_ID=$0
+local APP_TOKEN=$1
 
 curl \
 -F "status=2" \
 -F "notify=1" \
 -F "ipa=@$CIRCLE_ARTIFACTS/outputs/apk/app-debug.apk" \
--H "X-HockeyAppToken: e07db807b6344a6d8e65f1c5a8c0b4eb" \
-https://rink.hockeyapp.net/api/2/apps/29d2a836383d4fda977697a8754fc5de/app_versions/upload
+-H "X-HockeyAppToken: $APP_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/$APP_ID/app_versions/upload
